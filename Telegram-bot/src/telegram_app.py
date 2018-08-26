f25644751b21476be8bc5bc54186ab13f542cd58
Javier162380 <javierllorente16@gmail.com>
2018-08-26 22:39:30 +0200

    bot v2 change serverless.yml and function

--- Telegram-bot/src/telegram_app.py
+++ Telegram-bot/src/telegram_app.py
@@ -4,7 +4,7 @@ import os
 import sys
 from dotenv import load_dotenv,find_dotenv
 from telebot import TeleBot
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from src.yandex_api import yandex_transaltor
 from src.telegram_object import TelegramMessageObject
 
@@ -16,22 +16,24 @@ bot = TeleBot(os.getenv("telegram_token"))
 yandex_object = yandex_transaltor(os.getenv("yandex_api_key"))
 yandex_languages = yandex_object.get_all_available_languages(language_id='en')
 
+OK_RESPONSE = {"statusCode": 200, "headers": {}, "body": "Succesfull Requests"}
+FAILED_RESPONSE = {"statusCode": 404, "headers": {}, 
+                    "body": "Something goes wrong"}
 
 def proccess_lambda_headers(headers, headers_key):
     python_string = headers.get(headers_key)
     if python_string:
         return json.loads(python_string.replace("'", ""))
 
-
 def parse_translator(text):
     message_arguments = text.split(' ')
-    if len(message_arguments) >= 3:
-        target_command = message_arguments[0]
-        target_language = message_arguments[1]
-        message = ' '.join(message for message in message_arguments[2:])
+    if len(message_arguments) >= 2:
+        target_language = message_arguments[0]
+        message = ' '.join(message for message in message_arguments[1:])
         message_language = yandex_object.detect_language(message)
-        message_translate = yandex_object.translate_message(message=message, language_to_translate=target_language,
-                                                            message_language=message_language)
+        message_translate = yandex_object.translate_message(message=message, 
+                                            language_to_translate=target_language,
+                                            message_language=message_language)
         if message_translate:
             return message_translate
 
@@ -43,17 +45,17 @@ def send_languages(message_id,chat_id, message):
     yandex_languages_text = ''.join(f'{key}––>{value}\n'
                                      for key, value in yandex_languages.items())
 
-    bot.send_message(reply_to_message_id=message_id, chat_id=chat_id, text=yandex_languages_text)
+    bot.send_message(reply_to_message_id=message_id, chat_id=chat_id, 
+                      text=yandex_languages_text)
 
 def send_help(message_id,chat_id,message):
     help_message = f'Translate a message to the language you want, follow this steps:\n' \
-                   f'\t\t 1.Use the Translate command\n' \
-                   f'\t\t 2.Choose your target translate language in the two digits code.\n' \
+                   f'\t\t 1.Choose your target translate language in the two digits code.\n' \
                    f'\t\t\t\t To see all the differents languages codes use /languages command.\n' \
                    f'\t\t\t\t To see all the available translations use /translations command.\n'\
-                   f'\t\t 3.Write your message.\n' \
-                   f'The message should follow the following structure Translate LANGUAGECODE message.\n' \
-                   f'just as an example: Translate en eres tonto si estas utilizando este bot.'
+                   f'\t\t 2.Write your message.\n' \
+                   f'The message should follow the following structure LANGUAGECODE message.\n' \
+                   f'just as an example: en eres tonto si estas utilizando este bot.'
     bot.send_message(reply_to_message_id=message_id, chat_id=chat_id, text=help_message)
 
 
@@ -61,13 +63,15 @@ def send_translation(message_id, chat_id, message):
     translations = yandex_object.get_all_available_translations()
     if translations:
         reply_message = ','.join(translation for translation in translations)
-        bot.send_message(reply_to_message_id=message_id, chat_id=chat_id, text=reply_message)
+        bot.send_message(reply_to_message_id=message_id, chat_id=chat_id, 
+                          text=reply_message)
 
 
 def translate_message(message_id, chat_id, message):
     translate_message = parse_translator(message)
     if translate_message:
-        bot.send_message(reply_to_message_id=message_id,chat_id=chat_id, text=translate_message)
+        bot.send_message(reply_to_message_id=message_id,chat_id=chat_id, 
+                         text=translate_message)
 
 def telegram_execute_method(message_id,chat_id, message):
     telegram_function =  {'/start': send_welcome,'/languages': send_languages,
@@ -87,4 +91,5 @@ def telegram_trigger(event, context):
         if telegram_object:
             telegram_execute_method(message=telegram_object.text, chat_id=telegram_object.chat_id,
                                     message_id=telegram_object.message_id)
-    return {"statusCode": 200, "headers": {}, "body": "Succesfull Requests"}
+            return OK_RESPONSE
+    return FAILED_RESPONSE
--- Telegram-bot/src/yandex_api.py
+++ Telegram-bot/src/yandex_api.py
@@ -11,14 +11,16 @@ class yandex_transaltor():
         return message[:max_limit]
 
     def detect_language(self, message, max_limit=10000):
-        lang_detection = self.session.post(f'https://translate.yandex.net/api/v1.5/tr.json/detect'
+        lang_detection = self.session.post(f'https://translate.yandex.net/api/'
+                                           f'v1.5/tr.json/detect'
                                            f'?key={self.yandex_api_key}'
-                                           f'&text={self.truncate_message(message, max_limit)}')
+                           f'&text={self.truncate_message(message, max_limit)}')
         if lang_detection.status_code == 200:
             return lang_detection.json()['lang']
 
     def get_languages(self, language_id=None):
-        language_end_point = f'https://translate.yandex.net/api/v1.5/tr.json/getLangs?key={self.yandex_api_key}'
+        language_end_point = f'https://translate.yandex.net/api/v1.5/tr.json/'\
+                            f'getLangs?key={self.yandex_api_key}'
         if language_id:
             language_end_point = f'{language_end_point}&ui={language_id}'
         languages = self.session.get(language_end_point)
@@ -36,9 +38,11 @@ class yandex_transaltor():
             return languages_information['dirs'] if 'dirs' in languages_information.keys() else None
 
     def translate_message(self, message, language_to_translate, message_language):
-        translate_message = self.session.post(f'https://translate.yandex.net/api/v1.5/tr.json/translate'
+        translate_message = self.session.post(f'https://translate.yandex.net/'
+                                              f'api/v1.5/tr.json/translate'
                                               f'?key={self.yandex_api_key}'
                                               f'&text={self.truncate_message(message)}'
-                                              f'&lang={message_language}-{language_to_translate}')
+                                              f'&lang={message_language}-'
+                                              f'{language_to_translate}')
         if translate_message.status_code == 200:
             return translate_message.json()['text']
